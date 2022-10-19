5b758f914d3519674f79274191805abbef25a2ff
diver <abrarov9@gmail.com>
2022-10-19 15:34:37 +0300

    Lesson 4. User login and logout

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -20,9 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-opo#y%39&!v@&*6&lvpizu-^_b6&w(*k)e0ny0dj%ll_48nj$t"
-)
+SECRET_KEY = "django-insecure-opo#y%39&!v@&*6&lvpizu-^_b6&w(*k)e0ny0dj%ll_48nj$t"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -33,18 +31,18 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'mainapp',
-    'authnapp',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "mainapp",
+    "authnapp",
 ]
 
 # Auth model
-AUTH_USER_MODEL = 'authnapp.ShopUser'
+AUTH_USER_MODEL = "authnapp.ShopUser"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
